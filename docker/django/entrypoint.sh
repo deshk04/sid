52cc91c214a56616dec7b2b316979dff096434c3
Desh K <deshk76@gmail.com>
2021-05-12 17:07:15 +0800

    docker changes to django via entrypoint

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -2,4 +2,5 @@ echo "Let's create django environment settings"
 skey=`dd if=/dev/urandom bs=45 count=1 | base64`
 echo "DJANGO_SECRET_KEY=$skey" > /sid/config/.env
 cat /etc/environment >> ~/.bashrc
-uwsgi --emperor /sid/config/uwsgi/sid.uwsgi.ini
+cd /sid/src/django/web
+python manage runserver 0.0.0.0:8080
