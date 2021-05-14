9d0995e38165f1819daf46bcf1a84d8e0415a66a
Desh K <deshk76@gmail.com>
2021-05-14 22:50:25 +0800

    angular docker changes

--- docker/angular/entrypoint.sh
+++ docker/angular/entrypoint.sh
@@ -1,3 +1,11 @@
 cd /sid/src/angular/ng-sid/
-npm install
+# npm install
+if [ -d "/app/node_modules" ] 
+then
+    if [ -d "/sid/src/angular/ng-sid/node_modules" ]; then rm -Rf "/sid/src/angular/ng-sid/node_modules"; fi
+    ln -s /app/node_modules .
+else
+    echo "Error: Directory /app/node_modules doesn't exits"
+fi
+# while true; do sleep 2; done
 ng serve --port 4200 --host 0.0.0.0 --proxy-config proxy.conf.json
