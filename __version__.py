05f7ca483ad90dd348e645f392db10025f040209
Desh K <deshk76@gmail.com>
2021-05-25 21:00:57 +0800

    version file details update

--- __version__.py
+++ __version__.py
@@ -4,9 +4,9 @@ This file taken from the requests library
 """
 
 __title__ = 'sid'
-__description__ = 'A basic ETL Tool for salesforce'
+__description__ = 'A simple ETL Tool for salesforce'
 __url__ = 'https://github.com/deshk04/sid'
-__version__ = '0.2'
+__version__ = '1.0 (Beta)'
 __author__ = 'Desh K'
 __author_email__ = 'maildesh@gmail.com'
 __license__ = 'Apache 2.0'
